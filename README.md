a3cef7da6b4313991898924878e8344428b1847c
peteryu54089 <peteryu54089@gmail.com>
2022-03-17 18:44:47 -0500

    init

---
+++ README.md
@@ -0,0 +1,142 @@
+# Exercise 07.1 - Construction and Testing
+## _CS4320/7320 Software Engineering_
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+
+## Run
+
+```sh
+python3 -m pytest
+```
+
+## Restore Data
+
+```sh
+python3 RestoreData.py
+```
+
+## Features
+
+`passed` login - System.py
+`passed` check_password - System.py
+`failed` change_grade - Staff.py
+```sh
+self.users[user]['courses'][course][assignment]['grade'] = 0
+```
+> should be
+
+```sh
+self.users[user]['courses'][course][assignment]['grade'] = grade
+```
+`passed` create_assignment - Staff.py
+`failed` add_student - Professor.py
+```sh
+self.users[self.name]['courses'][course] = assignments
+```
+> should be
+
+```sh
+self.users[name]['courses'][course] = assignments
+```
+`passed` drop_student - Professor.py
+`failed` submit_assignment - Student.py
+```sh
+due_date = self.all_courses['comp_sci']['assignments'][assignment_name]["due_date"]
+```
+> should be
+
+```sh
+due_date = self.all_courses[course]['assignments'][assignment_name]["due_date"]
+```
+`failed` check_ontime - Student.py
+```sh
+return True
+```
+> should be
+
+```sh
+return SomeDateComparisonFunction(submission_date, due_date)
+```
+`passed?` `failed?` check_grades - Student.py
+> I'm not sure if this is a mistake by the professor because the "grade" key value is misspelled as "Grade" in some data, which causes a "KeyError". So I split it into two functions to test.
+
+*hdjsr7's assignment2 of cloud_computing*
+```sh
+"hdjsr7":{
+    "courses":{
+        "cloud_computing":{
+            "assignment1":{
+                "grade":100,
+                "submission_date":"1/3/20",
+                "submission":"Blah Blah Blah",
+                "ontime":true
+            },
+            "assignment2":{
+                "Grade":100,
+                "Submission Data":"2/3/20",
+                "Submission":"Blah2 Blah2 Blah2",
+                "ontime":true
+            }
+        },
+        ...
+    },
+    "password":"pass1234",
+    "role":"student"
+}
+```
+*yted91's assignment2 of cloud_computing*
+```sh
+"yted91":{
+    "courses":{
+        "cloud_computing":{
+            "assignment1":{
+                "grade":3,
+                "submission_date":"1/7/20",
+                "submission":"Blah Blah Blah",
+                "ontime":false
+            },
+            "assignment2":{
+                "Grade":5,
+                "Submission Data":"2/7/20",
+                "Submission":"Blah2 Blah2 Blah2",
+                "ontime":false
+            }
+        },
+        ...
+    },
+    "password":"imoutofpasswordnames",
+    "role":"student"
+}
+```
+`failed` view_assignments - Student.py
+```sh
+course = self.all_courses['comp_sci']['assignments']
+```
+> should be
+
+```sh
+course = self.all_courses[course]['assignments']
+```
+`failed` check_password_with_unexisted_user - System.py
+> When an incorrect username is entered, the system should return an account password error, not a crash.
+
+`failed` create_assignment_with_wrong_ta - Staff.py
+> TAs should not have the permission to create assignments that are not part of their courses.
+
+`failed` create_assignment_with_wrong_professor - Staff.py
+> Professors should not have the permission to create assignments that are not part of their courses.
+
+`failed` drop_student_with_wrong_professor - Professor.py
+> A professor should not have the permission to drop students from a course that is not his.
+
+`failed` check_grades_with_wrong_ta - Staff.py
+> TAs should not have the permission to check grades that are not part of their courses.
+
+`failed` check_grades_with_wrong_professor - Staff.py
+> Professors should not have the permission to check grades that are not part of their courses.
+
+## License
+
+MIT
+
+**Free Software, Hell Yeah!**
---
+++ example_test.py
@@ -0,0 +1,175 @@
+import pytest
+import System
+import RestoreData
+
+def test_login(grading_system):
+    name = 'saab'
+    courses = ['comp_sci']
+    password = 'boomr345'
+    grading_system.login(name, password)
+    assert grading_system.usr.name == name
+    assert grading_system.usr.courses == courses
+    assert grading_system.usr.password == password
+
+def test_check_password(grading_system):
+    name = 'saab'
+    password = 'boomr345'
+    assert grading_system.check_password(name, password) == True
+    password = 'error'
+    assert grading_system.check_password(name, password) == False
+
+# failed
+def test_change_grade(grading_system):
+    name = 'saab'
+    password = 'boomr345'
+    grading_system.login(name, password)
+    name = 'akend3'
+    course = 'comp_sci'
+    assignment = 'assignment1'
+    grade = 100
+    grading_system.usr.change_grade(name, course, assignment, grade)
+    assert grading_system.users[name]['courses'][course][assignment]['grade'] == grade
+
+def test_create_assignment(grading_system):
+    name = 'saab'
+    password = 'boomr345'
+    grading_system.login(name, password)
+    assignment = 'assignment3'
+    due_date = '3/19/22'
+    course = 'comp_sci'
+    count = len(grading_system.courses[course]['assignments'])
+    grading_system.usr.create_assignment(assignment, due_date, course)
+    assert len(grading_system.courses[course]['assignments']) == count + 1
+
+# failed
+def test_add_student(grading_system):
+    name = 'saab'
+    password = 'boomr345'
+    grading_system.login(name, password)
+    name = 'hdjsr7'
+    course = 'comp_sci'
+    count = len(grading_system.users[name]['courses'])
+    grading_system.usr.add_student(name, course)
+    assert len(grading_system.users[name]['courses']) == count + 1
+
+def test_drop_student(grading_system):
+    name = 'saab'
+    password = 'boomr345'
+    grading_system.login(name, password)
+    name = 'akend3'
+    course = 'comp_sci'
+    count = len(grading_system.users[name]['courses'])
+    grading_system.usr.drop_student(name, course)
+    assert len(grading_system.users[name]['courses']) == count - 1
+
+# failed
+def test_submit_assignment(grading_system):
+    name = 'hdjsr7'
+    password = 'pass1234'
+    grading_system.login(name, password)
+    course = 'cloud_computing'
+    assignment = 'assignment1'
+    submission = 'new submission'
+    submission_date = '1/4/20'
+    grading_system.usr.submit_assignment(course, assignment, submission, submission_date)
+    assert grading_system.users[name]['courses'][course][assignment]['grade'] == 'N/A'
+    assert grading_system.users[name]['courses'][course][assignment]['submission_date'] == submission_date
+    assert grading_system.users[name]['courses'][course][assignment]['submission'] == submission
+    assert grading_system.users[name]['courses'][course][assignment]['ontime'] == False
+
+# failed
+def test_check_ontime(grading_system):
+    name = 'hdjsr7'
+    password = 'pass1234'
+    grading_system.login(name, password)
+    submission_date = '1/4/20'
+    due_date = '1/3/20'
+    assert grading_system.usr.check_ontime(submission_date, due_date) == False
+
+def test_check_grades(grading_system):
+    name = 'yted91'
+    password = 'imoutofpasswordnames'
+    grading_system.login(name, password)
+    course = 'software_engineering'
+    assert grading_system.usr.check_grades(course) == [['assignment1', 43], ['assignment2', 22]]
+
+# failed
+def test_check_grades_with_key_error(grading_system):
+    name = 'yted91'
+    password = 'imoutofpasswordnames'
+    grading_system.login(name, password)
+    course = 'cloud_computing'
+    assert grading_system.usr.check_grades(course) == [['assignment1', 3], ['assignment2', 5]]
+
+# failed
+def test_view_assignments(grading_system):
+    name = 'yted91'
+    password = 'imoutofpasswordnames'
+    grading_system.login(name, password)
+    course = 'software_engineering'
+    assert grading_system.usr.view_assignments(course) == [['assignment1', '1/1/20'], ['assignment2', '2/1/20']]
+
+# failed
+def test_check_password_with_unexisted_user(grading_system):
+    name = 'unexisted'
+    password = 'boomr345'
+    assert grading_system.check_password(name, password) == False
+
+# failed
+def test_create_assignment_with_wrong_ta(grading_system):
+    name = 'cmhbf5' # cloud_computing, software_engineering
+    password = 'bestTA'
+    grading_system.login(name, password)
+    assignment = 'assignment3'
+    due_date = '3/19/22'
+    course = 'databases'
+    count = len(grading_system.courses[course]['assignments'])
+    grading_system.usr.create_assignment(assignment, due_date, course)
+    assert len(grading_system.courses[course]['assignments']) == count
+
+# failed
+def test_create_assignment_with_wrong_professor(grading_system):
+    name = 'saab' # comp_sci
+    password = 'boomr345'
+    grading_system.login(name, password)
+    assignment = 'assignment3'
+    due_date = '3/19/22'
+    course = 'cloud_computing'
+    count = len(grading_system.courses[course]['assignments'])
+    grading_system.usr.create_assignment(assignment, due_date, course)
+    assert len(grading_system.courses[course]['assignments']) == count
+
+# failed
+def test_drop_student_with_wrong_professor(grading_system):
+    name = 'saab' # comp_sci
+    password = 'boomr345'
+    grading_system.login(name, password)
+    name = 'hdjsr7'
+    course = 'cloud_computing'
+    count = len(grading_system.users[name]['courses'])
+    grading_system.usr.drop_student(name, course)
+    assert len(grading_system.users[name]['courses']) == count
+
+# failed
+def test_check_grades_with_wrong_ta(grading_system):
+    name = 'cmhbf5' # cloud_computing, software_engineering
+    password = 'bestTA'
+    grading_system.login(name, password)
+    name = 'akend3'
+    course = 'databases'
+    assert grading_system.usr.check_grades(name, course) == []
+
+# failed
+def test_check_grades_with_wrong_professor(grading_system):
+    name = 'saab' # comp_sci
+    password = 'boomr345'
+    grading_system.login(name, password)
+    name = 'akend3'
+    course = 'databases'
+    assert grading_system.usr.check_grades(name, course) == []
+
+@pytest.fixture
+def grading_system():
+    gradingSystem = System.System()
+    gradingSystem.load_data()
+    return gradingSystem
